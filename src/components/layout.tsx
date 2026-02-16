961f73d917ab3d40a06d6791944fb25bf205b540
harshit kudhial <122823756+harshitnub077@users.noreply.github.com>
2026-02-16 16:18:03 +0530

    chore: temporary rename to fix git casing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react'
-import Header from './Header'
+import Header from './TempHeader'
 
 const layout = ({ children }: PropsWithChildren) => {
     return (
