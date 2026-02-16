5e37d6f07ed940d9c8ecedd2ad1e4f2ecd7b3d1f
harshit kudhial <122823756+harshitnub077@users.noreply.github.com>
2026-02-16 11:44:35 +0530

    Fix mixed content and secure .env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,9 +1,7 @@
-import { Import } from "lucide-react";
-
 export const API_CONFIG = {
-    
-    BASE_URL: "http://api.openweathermap.org/geo/1.0/direct",
-    GEO: "http://api.openweathermap.org/geo/1.0/reverse",
+
+    BASE_URL: "https://api.openweathermap.org/geo/1.0/direct",
+    GEO: "https://api.openweathermap.org/geo/1.0/reverse",
     API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
     DEFAULT_PARAMS: {
         units: "metric",
